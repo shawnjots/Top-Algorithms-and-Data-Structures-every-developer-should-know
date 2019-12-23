27b0cdc3b6464ff042aeba97e5ebe310f897e88c
Shawn Majoto <shawnjots@gmail.com>
2019-12-23 11:55:27 +0200

    Linked List with Dependency Injection initial commit

---
+++ Linked List/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List
+{
+	public class Node
+	{
+		public int Value { get; set; }
+		public Node Next { get; set; }
+	}
+}
---
+++ Linked List/Program.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using System;
+
+namespace Linked_List
+{
+	class Program
+	{
+		public static IContainer Container;
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Linked Lists");
+
+			ContainerBuilder builder = new ContainerBuilder();
+			builder.RegisterType<Linked_List.Node>();
+			Container = builder.Build();
+
+			Node first = Container.Resolve<Linked_List.Node>();
+			first.Value = 5; 
+
+			Node second = Container.Resolve<Linked_List.Node>();
+			second.Value = 7; first.Next = second;
+
+			Node third = Container.Resolve<Linked_List.Node>();
+			third.Value = 9; second.Next = third;
+
+		}
+	}
+}
